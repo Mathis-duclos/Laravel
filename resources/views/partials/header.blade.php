30b281d56b81c889e47d6592ba0a698f81e6eaad
Mathis Duclos <mathis.duclos@icloud.com>
2025-09-26 11:41:38 +0200

    modification de l'affichage de la PP dans la sidebar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -17,16 +17,24 @@
                 @endauth
             </div>
 
-            @if(Auth::user()->avatar)
-    <img src="{{ Auth::user()->avatar }}" alt="Avatar"
-         class="rounded-full w-10 h-10 object-cover">
-@else
-    <img src="/images/default-avatar.png" alt="Avatar"
-         class="rounded-full w-10 h-10 object-cover">
-@endif
-
+            
             <div class="nav-bottom">
                 <hr class="nav-separator">
+                {{-- Avatar utilisateur et username --}}
+                @auth
+                    <div class="user-info">
+                        @if(Auth::user()->avatar)
+                            <img src="{{ Auth::user()->avatar }}" alt="Avatar">
+                        @else
+                            <img src="/images/default-avatar.png" alt="Avatar">
+                        @endif
+                        <span class="username">{{ Auth::user()->name }}</span>
+                    </div>
+                @else
+                    <img src="/images/default-avatar.png" alt="Avatar"
+                         class="rounded-full w-10 h-10 object-cover">
+                @endauth
+
                 @guest
                     <a href="{{ route('login') }}" class="btn btn-sm btn-outline-primary">Connexion</a>
                     @if (Route::has('register'))
@@ -35,8 +43,6 @@
                 @endguest
 
                 @auth
-                    <span class="username">{{ Auth::user()->name }}</span>
-
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
                         <button type="submit" class="btn btn-sm btn-danger">Déconnexion</button>
